2b428f415d17e8d2a2f917992b3f08f9e31aa32c
DATTB09 <22022172@vnu.edu.vn>
2024-11-15 09:38:52 +0700

    reader
    dat

--- src/main/java/library/Reader.java
+++ src/main/java/library/Reader.java
@@ -1,18 +1,63 @@
+//package library;
+//
+//import java.util.Scanner;
+//
+//public class Reader
+//{
+//    private static final Scanner sc = new Scanner(System.in);
+//
+//    public static int readInt()
+//    {
+//        return sc.nextInt();
+//    }
+//
+//    public static String readLine()
+//    {
+//        return sc.nextLine().trim();
+//    }
+//}
 package library;
 
-import java.util.Scanner;
+public class Reader {
+    private int readerID;
+    private String readerName;
+    private String email;
+    private String phoneNumber;
 
-public class Reader
-{
-    private static final Scanner sc = new Scanner(System.in);
-
-    public static int readInt()
-    {
-        return sc.nextInt();
+    public Reader(int readerID, String readerName, String email, String phoneNumber) {
+        this.readerID = readerID;
+        this.readerName = readerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
-    public static String readLine()
-    {
-        return sc.nextLine().trim();
+    // Getters
+    public int getReaderID() {
+        return readerID;
+    }
+
+    public String getReaderName() {
+        return readerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setReaderName(String readerName) {
+        this.readerName = readerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
